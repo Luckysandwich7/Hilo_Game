92a5718474a9067f254bb3b010a1f2327720bef9
Luckysandwich7 <90877920+Luckysandwich7@users.noreply.github.com>
2022-01-29 10:39:02 -0700

    Add files via upload

---
+++ import_card.py
@@ -0,0 +1,32 @@
+import card
+class Director:
+
+def __init__(self):
+    self.score = 0
+    self.user_guess = ""
+
+def start_game(self):
+    while score:
+        card = self.get_inputs()
+        self.do_updates(card)
+        self.do_outputs()
+
+def get_inputs(self):
+    current_card = Card()
+    current_card.get_current_card()
+    card = current_card.current_card
+    print("This card is:  {card} ")
+    self.user_guess = input("Higher or lower? [h/1]: ")
+    return card
+
+def do_updates(self, current_card):
+    card = Card()
+    next_card.get_next_card()
+    print('Next card was: {card}')
+if current_card > next_card and user_guess == "l":
+    self.score += 100
+else:
+    self.score : self.score -= 75 ? self.score
+
+def do_outputs():
+    print(f"Your score is: {self.score}")
